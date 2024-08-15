e81e87386f1d57e416ffc9b3500fc90b5d1ac234
Yehoshua Cohen <100349784+Shuicohen@users.noreply.github.com>
2024-08-15 14:22:34 +0300

    Add files via upload

---
+++ week2/day5/exercise/week2_day5_daily_challenge_2.py
@@ -0,0 +1,15 @@
+import requests
+import time
+
+def measure_load_time(url):
+    start_time = time.time()
+    response = requests.get(url)
+    end_time = time.time()
+    load_time = end_time - start_time
+    return load_time
+
+websites = ['https://www.google.com', 'https://www.ynet.co.il', 'https://www.imdb.com']
+
+for site in websites:
+    load_time = measure_load_time(site)
+    print(f"Load time for {site}: {load_time:.2f} seconds")
