e6eb85a00726c8854e7c7f58228091db4eff943a
Yehoshua Cohen <100349784+Shuicohen@users.noreply.github.com>
2024-08-19 15:13:36 +0300

    Add files via upload

---
+++ week3/day2/exercise/week3_day2_daily_challenge.sql
@@ -0,0 +1,11 @@
+-- Question 1:
+-- will return 0
+
+-- Question 2:
+-- will return 2  (for the rows with id values 6 and 7)
+
+-- Question 3:
+-- will return 0
+
+-- Question 4:
+-- will return 2  (for the rows with id values 6 and 7)
