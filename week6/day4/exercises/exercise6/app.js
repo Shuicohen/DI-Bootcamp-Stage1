40d4d97cc38bbf9b64099e7255730748f214cf7f
Yehoshua Cohen <100349784+Shuicohen@users.noreply.github.com>
2024-09-11 14:53:53 +0300

    Add files via upload

---
+++ week6/day4/exercises/exercise6/app.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+// Create colorful messages using chalk
+console.log(chalk.blue('Hello, World!'));  // Blue text
+console.log(chalk.green.bold('This is a bold green message.'));  // Bold green text
+console.log(chalk.red.underline('This is an underlined red message.'));  // Underlined red text
+console.log(chalk.bgYellow.black('This is a black text with yellow background!'));  // Black text with yellow background
