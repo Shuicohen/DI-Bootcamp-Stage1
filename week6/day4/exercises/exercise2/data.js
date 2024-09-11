d0e459ab2779361b0f3b20b0ac5bfa2eef5037b9
Yehoshua Cohen <100349784+Shuicohen@users.noreply.github.com>
2024-09-11 14:45:58 +0300

    Add files via upload

---
+++ week6/day4/exercises/exercise2/data.js
@@ -0,0 +1,7 @@
+export const people = [
+    { name: 'John', age: 30, location: 'New York' },
+    { name: 'Jane', age: 25, location: 'Los Angeles' },
+    { name: 'Mike', age: 35, location: 'Chicago' },
+    { name: 'Sarah', age: 28, location: 'Houston' },
+    { name: 'Tom', age: 40, location: 'Seattle' }
+];
